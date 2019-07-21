ae1036cf1105037cf91e3a88c3f5ca6587a693f1
Rigin Oommen <roommen@redhat.com>
2019-07-21 22:19:03 +0530

    Fixed the credentials

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.get("/", (req ,res)=> {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-connect(`mongodb://graphqluser:graphqlpwd@ds253537.mlab.com:53537/heroku_v789676v`, { useNewUrlParser: true }).then(() => console.log(`Established the Database connection`));
+connect(`mongodb://graphqluser:graphqlpwd1@ds253537.mlab.com:53537/heroku_v789676v`, { useNewUrlParser: true }).then(() => console.log(`Established the Database connection`));
 
 const options: Options = {
   port: process.env.port || 8000,
